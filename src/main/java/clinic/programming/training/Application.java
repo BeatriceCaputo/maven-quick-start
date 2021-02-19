f9098b74e26ab4d8e66da98e98455cd6ee1cb012
CaBeat <bcaputo92@gmail.com>
2021-02-19 12:10:37 +0100

    Add dependences and updating java code

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,15 +1,37 @@
 
 package clinic.programming.training;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
-    
+
+    public int countWords(String words){
+      String[] sepateWords = StringUtils.split(words, ' ');
+      return (sepateWords == null) ? 0 : sepateWords.length;
+
+    }
+
+    public void greet (){
+      List<String> greetings = new ArrayList<String>();
+      greetings.add("Hello");
+
+      for (String greeting: greetings){
+        System.out.println("greetings: " + greeting);
+      }
+    }
+
     public Application() {
         System.out.println ("Inside Application");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
+    	 System.out.println ("Starting Application");
+	     Application app = new Application();
+       app.greet();
+       System.out.println("Count Words " + app.countWords("I have four words"));
+
     }
 }
